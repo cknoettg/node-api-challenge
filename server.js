cdc236999d76fc7ff1401639e7a8c0b0fed123cd
cknoettg <cknoettg@yahoo.com>
2020-07-18 01:51:05 -0400

    Reorganized project, added server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+// server.use('/api/users', userRouter);
+// server.use('/api/post', postRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+});
+  
+//logger function
+function logger(req, res, next) {
+    console.log(`Method: ${req.method} Url: ${req.originalUrl} Time: ${Date.now()}`);
+    next();
+}
+  
+module.exports = server;
